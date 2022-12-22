20213b604eacf0b8d1ad8b85bc85c03dcff9d4f8
DSBuyanov <inquis87@gmail.com>
2022-12-22 13:25:51 +0300

    1.5

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2-> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write($"Введите количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] newArray = new int[m];
+
+void InputNumbers(int m)
+{
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введите {i+1} число: ");
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int Comparison(int[] newArray)
+{
+  int count = 0;
+  for (int i = 0; i < newArray.Length; i++)
+  {
+    if(newArray[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(m);
+
+Console.WriteLine(Comparison(newArray));
